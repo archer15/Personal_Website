dfe8ef4f73d40f2c354a171bd9205ecef581d64f
Joshua Archer <joshua.archer@students.mq.edu.au>
2021-05-18 15:37:01 +1000

    fixes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import './App.css';
-import React, { useState, useEffect } from "react";
+//import React, { useState, useEffect } from "react";
 import {
   BrowserRouter as Router, Switch, Route, Link
 } from "react-router-dom"
 import About from './pages/about.js'
 import Home from './pages/home.js'
 import Documents from './pages/documents.js'
-import AOS from 'aos';
 import Projects from './pages/projects.js'
 
-
-// ..
-AOS.init();
 
 const padding = {
   padding: 10
@@ -48,11 +44,8 @@ const App = () => {
           </Router>
         </div>
     
-    <div className="App-footer"><link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />Thank you for visiting my personal website. </div>
-    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
-  <script>
-    AOS.init();
-  </script></div>
+    
+ </div>
   );
 }
 
