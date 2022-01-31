4138ed56c8afde15c9248d49b77e604c63c41b85
Joshua Archer <joshua.archer@students.mq.edu.au>
2022-01-31 15:01:52 +1100

    Maintanence screen

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@
 import React from 'react'
 
 const App = () => {
+  const mystyle = {
+    color: "white",
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    
+    backgroundColor: "DodgerBlue",
+    padding: "10px",
+    fontFamily: "Arial"
+  };
   return (
     <div className="App-main">
-      
-      <div className="App-footer">Thanks for visiting my personal website.</div>
+      <p style={{fontSize: "40px"}}>This website is currently under maintanence. Check back later for updates. Thank you. </p>
+      <div className="App-footer" style={mystyle}>Thanks for visiting my personal website.</div>
     
  </div>
   );
