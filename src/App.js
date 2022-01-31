378fa54acb45405c0d246e7d04be09bc7dd5cdef
Joshua Archer <joshua.archer@students.mq.edu.au>
2022-01-31 13:48:45 +1100

    Reset all of the website - restard project

--- src/App.js
+++ src/App.js
@@ -1,52 +1,10 @@
-import './App.css';
-//import React, { useState, useEffect } from "react";
-import {
-  BrowserRouter as Router, Switch, Route, Link
-} from "react-router-dom"
-import About from './pages/about.js'
-import Home from './pages/home.js'
-import JA_logo from './JA_logo.PNG'
-import Projects from './pages/projects.js'
-import my_cv from './pages/JoshuaArcher_CV_2021.pdf'
-import linkedin_image from './pages/linkedin.png'
-const padding = {
-  padding: 30
-}
+
+import React from 'react'
 
 const App = () => {
   return (
     <div className="App-main">
-      <div className="App-title">Portfolio. <img src={JA_logo} alt="Joshua Archer" width="40" height="35" className="logo_img">
-        </img></div>
-      <div className="App-router">
-          <Router>
-          
-          <div className="App-nav">
-            
-            <Link style={padding} to ="/"> Home </Link>
-            <Link style={padding} to ="/about"> About </Link>
-            <Link style={padding} to ="/projects"> Projects  </Link>
-            <a href= {my_cv} target="_blank" rel="noreferrer"> Resume <i className="fas fa-file"></i></a>
-          </div>
-          <div className="App-links">
-         
-            <a href="https://www.linkedin.com/in/joshua-archer/" target="_blank" rel="noreferrer"><img src={linkedin_image} width="40" height="40" className="img_link" alt="Logo" ></img></a>
-            
-          </div>
-          
-          <Switch>
-            <Route path="/about">
-            <About></About>
-            </Route>
-            <Route path="/projects">
-              <Projects></Projects>
-            </Route>
-            <Route path="/">
-                <Home></Home>
-            </Route>
-          </Switch>
-          </Router>
-        </div>
+      
       <div className="App-footer">Thanks for visiting my personal website.</div>
     
  </div>
